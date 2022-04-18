439032edb12c78c3a60489499316f599847ae267
Marvin obig <obig.marv@gmail.com>
2022-04-18 17:26:45 +0100

    add form data parsing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 const indexRouter = require("./routes/indexRouter");
 const recipesRouter = require("./routes/recipesRouter");
