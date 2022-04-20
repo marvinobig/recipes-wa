efd36324a18182eabc27eb280b9a560b1fb415f3
Marvin obig <obig.marv@gmail.com>
2022-04-20 18:10:30 +0100

    install multer

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const path = require("path");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
 const express = require("express");
 const app = express();
 
 dotenv.config();
 
+// database setup
 const db = process.env.MONGODB_URI;
 mongoose.connect(db, () => {
   console.log("Connected");
 });
 
+// ejs setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// use static files setup
 app.use(express.static(path.join(__dirname, "public")));
+
+//access to form body
 app.use(express.urlencoded({ extended: true }));
 
+//router files
 const indexRouter = require("./routes/indexRouter");
 const recipesRouter = require("./routes/recipesRouter");
 
