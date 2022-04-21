bfa6e4ca291eac0047df77712dd563b79872ff2f
Marvin obig <obig.marv@gmail.com>
2022-04-21 13:49:53 +0100

    add form validation

--- middleware/validators/formValidation.js
+++ middleware/validators/formValidation.js
@@ -1,5 +1,143 @@
-const { body, validationResult } = require("express-validator");
+const { body, validationResult, check } = require("express-validator");
 
-function validateRecipe(req, res, next) {}
+const validateRecipe = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is empty")
+    .isString()
+    .withMessage("Name should be letters")
+    .isLength({ min: 3 })
+    .withMessage("Name should be longer than 3 letters")
+    .trim()
+    .escape(),
+  body("description")
+    .notEmpty()
+    .withMessage("Description is empty")
+    .isString()
+    .withMessage("Description should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Description should be longer than 10 letters")
+    .trim()
+    .escape(),
+  body("time")
+    .notEmpty()
+    .withMessage("Time to make is empty")
+    .isInt({ min: 1 })
+    .withMessage("Time to make should be greater than 1")
+    .trim()
+    .escape(),
+  body("ingredients")
+    .notEmpty()
+    .withMessage("Ingredients is empty")
+    .isString()
+    .withMessage("Ingredients should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Ingredients should be longer than 10 letters")
+    .trim()
+    .escape(),
+  body("steps")
+    .notEmpty()
+    .withMessage("Steps is empty")
+    .isString()
+    .withMessage("Steps should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Steps should be longer than 10 letters")
+    .trim()
+    .escape(),
+  (req, res, next) => {
+    const err = validationResult(req);
+    console.log(err);
 
-module.exports = validateRecipe();
+    if (!err.isEmpty()) {
+      return res
+        .status(400)
+        .render("error", { title: "Form errors", errs: err });
+    } else {
+      next();
+    }
+  },
+];
+
+const validateUpdateRecipe = [
+  body("name")
+    .notEmpty()
+    .withMessage("Name is empty")
+    .isString()
+    .withMessage("Name should be letters")
+    .isLength({ min: 3 })
+    .withMessage("Name should be longer than 3 letters")
+    .trim()
+    .escape(),
+  body("description")
+    .notEmpty()
+    .withMessage("Description is empty")
+    .isString()
+    .withMessage("Description should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Description should be longer than 10 letters")
+    .trim()
+    .escape(),
+  body("time")
+    .notEmpty()
+    .withMessage("Time to make is empty")
+    .isInt({ min: 1 })
+    .withMessage("Time to make should be greater than 1")
+    .trim()
+    .escape(),
+  body("ingredients")
+    .notEmpty()
+    .withMessage("Ingredients is empty")
+    .isString()
+    .withMessage("Ingredients should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Ingredients should be longer than 10 letters")
+    .trim()
+    .escape(),
+  body("steps")
+    .notEmpty()
+    .withMessage("Steps is empty")
+    .isString()
+    .withMessage("Steps should be letters")
+    .isLength({ min: 10 })
+    .withMessage("Steps should be longer than 10 letters")
+    .trim()
+    .escape(),
+  (req, res, next) => {
+    const err = validationResult(req);
+
+    if (!err.isEmpty()) {
+      return res
+        .status(400)
+        .render("error", { title: "Form errors", errs: err });
+    } else {
+      next();
+    }
+  },
+];
+
+const validateUpdateDeletePassword = [
+  body("password")
+    .notEmpty()
+    .withMessage("Password is empty")
+    .matches("change")
+    .withMessage("Password is wrong")
+    .trim()
+    .escape(),
+  (req, res, next) => {
+    const err = validationResult(req);
+
+    if (!err.isEmpty()) {
+      return res
+        .status(400)
+        .render("error", { title: "Form errors", errs: err });
+    } else {
+      next();
+    }
+  },
+];
+
+module.exports = {
+  validateRecipe,
+  validateUpdateRecipe,
+  validateUpdateDeletePassword,
+};
