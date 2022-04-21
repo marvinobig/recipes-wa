6ff0469041ad82213fd8cd42b420d6af04eb1196
Marvin obig <obig.marv@gmail.com>
2022-04-21 21:22:16 +0100

    make fields required

--- models/recipeModel.js
+++ models/recipeModel.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const recipeSchema = new mongoose.Schema({
   img: String,
   name: { type: String, required: true },
-  category: String,
+  category: { type: String, required: true },
   description: { type: String, required: true },
-  time: Number,
-  ingredients: { type: Array, required: true },
-  steps: { type: Array, required: true },
+  time: { type: Number, required: true },
+  ingredients: { type: String, required: true },
+  steps: { type: String, required: true },
 });
 
 const recipe = mongoose.model("food_recipe", recipeSchema);
